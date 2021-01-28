e04a68aad9819c297a5c253e2a7280a8c3f89bd6
Mia Bueno <mia.bueno101@gmail.com>
2021-01-28 17:05:29 +1100

    learn how to get and set attributes with jquery

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-$("h1").css("color", "tomato");
+// get attribute
+console.log($("img").attr("src"));
+console.log($("a").attr("href"));
+console.log($("h1").attr("class"))
+
+// set attribute
+$("img").attr("src","https://i.pinimg.com/originals/14/f3/40/14f340ba15cfcc11faad3880e91ba219.jpg");
+$("a").attr("href", "http://bing.com")
