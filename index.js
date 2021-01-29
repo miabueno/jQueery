a6b61c0c3280f61b18554a4b22e6d9e453ad50f2
Mia Bueno <mia.bueno101@gmail.com>
2021-01-29 11:53:49 +1100

    experiment adding event listeners with jQuery

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 // get attribute
 console.log($("img").attr("src"));
 console.log($("a").attr("href"));
@@ -6,3 +8,41 @@ console.log($("h1").attr("class"))
 // set attribute
 $("img").attr("src","https://i.pinimg.com/originals/14/f3/40/14f340ba15cfcc11faad3880e91ba219.jpg");
 $("a").attr("href", "http://bing.com")
+
+*/
+
+// -----------------------------------------------
+// Section 14 - Adding Event Listeners with jQuery
+// -----------------------------------------------
+
+// Vanilla JS Version
+// When clicking on any button --> turn h1 to color purple
+/*
+var buttons = document.querySelectorAll("button");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(){
+        document.querySelector("h1").style.color = "purple";
+    });
+}
+*/
+
+// jQuery Version
+// When clicking on any button --> turn h1 to color purple
+$("button").click(function() {
+    $("h1").css("color", "purple");
+});
+
+// console log - keys that are pressed for input element
+$("input").keydown(function(event) {
+    console.log(event.key);
+});
+
+// CHALLENGE: When key is pressed, show in h1
+$(document).keydown(function(event) {
+    $("h1").text(event.key);
+});
+
+// using "on" to add event listeners
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "aqua");
+});
